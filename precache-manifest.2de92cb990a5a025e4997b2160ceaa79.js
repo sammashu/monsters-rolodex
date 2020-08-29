924e01235d4c0eac08735c902d83bef44d83998a
sammashu <sammashu@hotmail.com>
2020-08-29 18:57:15 -0400

    Updates

---
+++ precache-manifest.2de92cb990a5a025e4997b2160ceaa79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc1bdc25ab9cdadd39822990eed29b28",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ebc3cb3abcac425f5762",
+    "url": "/monsters-rolodex/static/css/main.ea94ff66.chunk.css"
+  },
+  {
+    "revision": "7b555debf535f32f782e",
+    "url": "/monsters-rolodex/static/js/2.9fc7c5d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.9fc7c5d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebc3cb3abcac425f5762",
+    "url": "/monsters-rolodex/static/js/main.4e60b993.chunk.js"
+  },
+  {
+    "revision": "dfba82531f669c6858b0",
+    "url": "/monsters-rolodex/static/js/runtime-main.589ddcef.js"
+  }
+]);
